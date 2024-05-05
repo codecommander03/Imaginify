6491b8888384f748674cad5116042609bf78e5c9
Aditya <codecommander03@gmail.com>
2024-05-05 16:10:10 +0530

    pages created for future use

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <p>Home</p>
+
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </>
+  );
+}
---
+++ src/app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransformatiosPage = () => {
+    return (
+        <div>TransformatiosPage</div>
+    )
+}
+
+export default TransformatiosPage
