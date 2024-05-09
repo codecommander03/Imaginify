b165da30a38edca24a0fcd35f27a9ccef7e302bb
Aditya <codecommander03@gmail.com>
2024-05-09 20:39:31 +0530

    user button removed

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
       <div>
         <p>Home</p>
-
-        <UserButton afterSignOutUrl="/" />
       </div>
     </>
   );
